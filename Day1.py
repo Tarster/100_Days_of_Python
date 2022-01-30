dc8f77c2edde02904809c220763f68da81de3c38
Tarun <sharmatarun537@gmail.com>
2022-01-30 17:50:06 +0530

    Day 1 complete

---
+++ Day1.py
@@ -0,0 +1,7 @@
+# Day1 Band name generator
+print("Welcome to the band name Generator.")
+print("What's name of the city you grew up in?")
+city_name = input()
+print("What's your pet's name?")
+pet_name = input()
+print(f"Your band name could be {city_name} {pet_name}")
